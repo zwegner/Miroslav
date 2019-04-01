fa4c065897ac44e01efb5341da17ec331564c4e0
Zach Wegner <zwegner@gmail.com>
2019-03-31 21:38:51 -0500

    Improve CLI a bit: add -c option for counting, and multiple input files

--- main.cpp
+++ main.cpp
@@ -5,14 +5,26 @@
 
 #include "miroslav.h"
 
+// hacky macro for easier argument chomping
+#define EAT_ARG() do { argc--; argv++; } while (0);
+
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " pattern file\n";
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " [-c] pattern file...\n";
         exit(1);
+    }
+    EAT_ARG();
+
+    // Option parsing
+    bool print_count = false;
+    if (!strcmp(argv[0], "-c")) {
+        print_count = true;
+        EAT_ARG();
     }
 
     // Parse the input regex
-    std::string pattern(argv[1]);
+    std::string pattern(argv[0]);
+    EAT_ARG();
     std::vector<std::tuple<uint8_t, uint32_t, uint32_t>> edges;
     uint32_t last_state = START_STATE;
     uint32_t state = 2;
@@ -34,10 +46,15 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    MatchHandlerPrintLine mh(true, false, true);
+    bool print_path = (argc > 1);
 
-    File f(argv[2]);
+    MatchHandlerPrintLine mh(print_path, !print_count, print_count);
 
-    Miroslav<MatchHandlerPrintLine> m(edges, mh); 
-    m.run(f);
+    while (argc > 0) {
+        File f(argv[0]);
+        EAT_ARG();
+
+        Miroslav<MatchHandlerPrintLine> m(edges, mh); 
+        m.run(f);
+    }
 }
