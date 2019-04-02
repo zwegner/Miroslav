647cacfc95d190c016fd881391c2f1abd2d895f6
Zach Wegner <zwegner@gmail.com>
2019-04-02 01:27:21 -0500

    Improve pattern support: add -f for reading patterns from a file, and try to not create any redundant states when building the NFA

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <sys/mman.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "miroslav.h"
 
@@ -10,7 +12,8 @@
 
 int main(int argc, char **argv) {
     if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " [-c] pattern file...\n";
+        std::cerr << "Usage: " << argv[0] << " [-c] pattern path...\n";
+        std::cerr << "       " << argv[0] << " [-c] -f pattern-file path...\n";
         exit(1);
     }
     EAT_ARG();
@@ -24,21 +27,55 @@ int main(int argc, char **argv) {
 
     // Parse the input regex
     std::string pattern(argv[0]);
-    EAT_ARG();
+    char sep = '|';
+    if (!strcmp(argv[0], "-f")) {
+        EAT_ARG();
+        std::ifstream f(argv[0]);
+        EAT_ARG();
+        std::stringstream buffer;
+        buffer << f.rdbuf();
+        pattern = buffer.str();
+        sep = '\n';
+    } else {
+        pattern = std::string(argv[0]);
+        EAT_ARG();
+    }
+
     std::vector<std::tuple<uint8_t, uint32_t, uint32_t>> edges;
     uint32_t last_state = START_STATE;
     uint32_t state = 2;
+    bool can_be_duplicate = true;
     for (uint32_t i = 0; i < pattern.length(); i++) {
         auto c = pattern[i];
 
-        if (c == '|')
+        if (c == sep) {
             last_state = START_STATE;
-        else if (i < pattern.length() - 1 && pattern[i + 1] != '|') {
-            edges.push_back(std::make_tuple(c, last_state, state));
-            last_state = state;
-            state += 1;
-        } else
-            edges.push_back(std::make_tuple(c, last_state, END_STATE));
+            can_be_duplicate = true;
+        } else {
+            // Add a new character to the pattern. Before we do, see if we can
+            // branch off a previous state that matches the substring
+            // up until now.
+            if (can_be_duplicate) {
+                uint8_t c2;
+                uint32_t f2, t2;
+                can_be_duplicate = false;
+                FOR_EACH_EDGE(c2, f2, t2, edges) {
+                    if (c2 == c && f2 == last_state && t2 != END_STATE) {
+                        can_be_duplicate = true;
+                        last_state = t2;
+                        break;
+                    }
+                }
+                if (can_be_duplicate)
+                    continue;
+            }
+            if (i < pattern.length() - 1 && pattern[i + 1] != sep) {
+                edges.push_back(std::make_tuple(c, last_state, state));
+                last_state = state;
+                state += 1;
+            } else
+                edges.push_back(std::make_tuple(c, last_state, END_STATE));
+        }
     }
 
     if (state > MAX_STATES) {
@@ -49,12 +86,12 @@ int main(int argc, char **argv) {
     bool print_path = (argc > 1);
 
     MatchHandlerPrintLine mh(print_path, !print_count, print_count);
+    Miroslav<MatchHandlerPrintLine> m(edges, mh); 
 
     while (argc > 0) {
         File f(argv[0]);
         EAT_ARG();
 
-        Miroslav<MatchHandlerPrintLine> m(edges, mh); 
         m.run(f);
     }
 }
